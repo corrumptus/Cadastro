4961de807891cbe6256cc2deeab2ec7b5a04b585
corrumptus <lazarinilucas42@gmail.com>
2024-03-05 17:31:53 -0300

    setting click listeners to the buttons and using viewBinding

--- app/src/main/java/lazarini/lucas/cadastro/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/cadastro/MainActivity.kt
@@ -1,11 +1,47 @@
 package lazarini.lucas.cadastro
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(activityMainBinding.getRoot())
+
+        activityMainBinding.salvarBt.setOnClickListener {
+            val formulario = Formulario(
+                activityMainBinding.nomeEt.text.toString(),
+                activityMainBinding.telefoneEt.text.toString(),
+                activityMainBinding.emailEt.text.toString(),
+                activityMainBinding.mailListCb.isChecked,
+                if (activityMainBinding.sexoRadMasc.isChecked)
+                    "Masculino"
+                else
+                    "Feminino",
+                activityMainBinding.cidadeEt.text.toString(),
+                activityMainBinding.ufSp.selectedItem.toString()
+            )
+
+            formulario.toString().also {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        activityMainBinding.limparBt.setOnClickListener {
+            activityMainBinding.nomeEt.setText("")
+            activityMainBinding.telefoneEt.setText("")
+            activityMainBinding.emailEt.setText("")
+            activityMainBinding.mailListCb.isChecked = false
+            activityMainBinding.sexoRadMasc.isChecked = true
+            activityMainBinding.cidadeEt.setText("")
+            activityMainBinding.ufSp.setSelection(0)
+        }
     }
 }
